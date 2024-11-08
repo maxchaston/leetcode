0d55587f80e278680df4b6c05ed2b678b79b547a
maxchaston <92534529+maxchaston@users.noreply.github.com>
2024-11-08 19:12:04 +0900

    Added improved C solution to problem 50 (Pow(x,n)).

--- solutions/0050 - Pow(x,n)/main.c
+++ solutions/0050 - Pow(x,n)/main.c
@@ -5,51 +5,31 @@
 #include <math.h>
 
 /**
-	 Initial attempt.
-	 Pretty basic.
-	 Multiplies by itself, getting to nearest power of 2 below remaining power needed.
-	 Then set the power back to zero and repeats.
+	 Second attempt.
+	 Ideal solution I think.
+	 Recursive solution, whittles down instead of counting up.
  **/
 
 double myPow(double x, long n)
 {
+	printf("x and n %f\t%d\n", x, n);
 	// zero case
 	if (n==0)
 		return 1;
 
 	// negative case
-	int is_neg = 0;
 	if (n<0)
-	{
-		is_neg = 1;
-		n = -n;
-	}
+		return myPow(1/x, -n); 
 
-	// base case
-	double sum = 1;
-	long remaining_index = n;
-	while (remaining_index!=0)
-	{
-		double tmp_sum = x;
-		printf("remaining index %d\n", remaining_index);
-		long running_index;
-		for (running_index = 2; running_index<remaining_index; running_index*=2)
-		{
-			printf("%d\t%d\n", running_index, n);
-			tmp_sum = tmp_sum*tmp_sum;
-		}
-		printf("sum %f\n", tmp_sum);
-		sum*=tmp_sum;
-		remaining_index-=running_index/2;
-	}
-	if (is_neg)
-		return 1/sum;
-	return sum;
+	if (n%2==1)
+		return x * myPow(x, n-1);
+
+	return myPow(x*x, n/2);
 }
 
 
 int main()
 {
-	printf("%f\n", myPow(2.0, -10));
+	printf("%f\n", myPow(2.0, 10));
 	return 0;
 }
