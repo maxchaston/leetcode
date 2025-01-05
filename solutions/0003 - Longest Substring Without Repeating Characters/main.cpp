abca34262f33fad19bae8c24e58e86dc5ad078f4
maxchaston <92534529+maxchaston@users.noreply.github.com>
2025-01-05 22:10:03 +0000

    Improved C++ solution to problem 3.

--- solutions/0003 - Longest Substring Without Repeating Characters/main.cpp
+++ solutions/0003 - Longest Substring Without Repeating Characters/main.cpp
@@ -1,40 +1,45 @@
 #include <string>
-#include <vector>
 #include <iostream>
 #include <unordered_map>
 
 using std::string, std::cout, std::endl;
 
-int length_of_continuous_run(string s) {
-	int count = 0;
-	std::unordered_map<char, int> found;
-	for (char c : s) {
-		if (found[c])
-			return count;
-		found[c] = 1;
-		count++;
-	}
-	return count;
-}
+/**
+	 Iterating through one character at a time.
+	 If new character is not part of the substring, add to map.
+	 If it is, keep shifting left side of sliding window and removing from map until a valid substring is found.
+	 Can store the index of the letter in the map.
+ **/
 
 int lengthOfLongestSubstring(string s) {
-	int len = 0;
-	for (int i=0; i<s.length(); i++) {
-		int thislen = length_of_continuous_run(s.substr(i));
-		if (thislen > len)
-			len = thislen;
+	int max_len = 0;
+	std::unordered_map<char, int> map;
+	int left = 0;
+	for (int right=0; right<s.length(); right++) {
+
+		if (map.count(s[right]) != 0) { // new value is already in the map
+			for (;left<=map[s[right]];left++) { // catch left side of window up to duplicate
+				map.erase(s[left]);
+			}
+			map[s[right]] = right;
+		}
+
+		else { // new value not in the map
+			max_len = (right+1-left)>max_len ? (right+1-left) : max_len;
+			map[s[right]] = right;
+		}
 	}
-	return len;
+	return max_len;
 }
 
 int main() {
-	cout << lengthOfLongestSubstring("pwwkew") << endl;
+	cout << lengthOfLongestSubstring("abcabcbb") << endl;
 	return 0;
 }
 
 /**
-	 Initial solution.
-	 Didn't place very well on the leetode histogram.
-	 Main problem is that it iterates over the same substrings multiple times.
-	 Improvement could be made with a sliding window, only checking the new and removing the old character.
+	 Second solution.
+	 Sliding window approach.
+	 Still didn't place very well on the leetode histogram.
+	 I assume this time it's because of the map.count() function being poorly optimised.
  **/
