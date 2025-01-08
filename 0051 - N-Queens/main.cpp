642788e86d29740d275bc9489bb8951c87c796d8
maxchaston <92534529+maxchaston@users.noreply.github.com>
2025-01-08 15:20:20 +0000

    Adjusted file structure.

--- 0051 - N-Queens/main.cpp
+++ 0051 - N-Queens/main.cpp
@@ -18,46 +18,33 @@ bool is_valid(int x, int y, std::vector<std::string> &board, int n) {
 			return false;
 	}
 
-	// TODO come back and clear these up, undefined behaviour
-	{
-		// check north-west diagonal
-		for (int i=y; i>=0; i--) {
-			if (x-(y-i) >= 0) {
-				if (board[i][x-(y-i)] == 'Q')
+	// check north-west diagonal
+	for (int i=y; i>=0; i--) {
+		if (x-(y-i) >= 0) {
+			if (board[i][x-(y-i)] == 'Q')
 				return false;
-			}
 		}
+	}
 
 
-		// check north-east diagonal
-		for (int i=y; i>=0; i--) {
-			if (x+(y-i) < n) {
-				if (board[i][x+(y-i)] == 'Q')
+	// check north-east diagonal
+	for (int i=y; i>=0; i--) {
+		if (x+(y-i) < n) {
+			if (board[i][x+(y-i)] == 'Q')
 				return false;
-			}
 		}
 	}
 	return true;
 }
 
-std::vector<std::string> test_board = {
-	"Q.....",
-	"......",
-	"......",
-	"......"
-};
-
-
-std::vector<std::vector<std::string>> solved_boards;
-
-void solve_recurse(std::vector<std::string> board, int y, int n) { // don't want passing by reference as we use the copy to manipulate
+void solve_recurse(std::vector<std::string> board, int y, int n, std::vector<std::vector<std::string>> &solved_boards) { // don't want passing by reference as we use the copy to manipulate
 	for (int i=0; i<n; i++) {
 		if (is_valid(i, y, board, n)) {
 			board[y][i] = 'Q';
 			if (y==n-1) {
 				solved_boards.push_back(board);
 			} else {
-				solve_recurse(board, y+1, n);
+				solve_recurse(board, y+1, n, solved_boards);
 			}
 			board[y][i] = '.';
 		}
@@ -65,24 +52,25 @@ void solve_recurse(std::vector<std::string> board, int y, int n) { // don't want
 }
 
 std::vector<std::vector<std::string>> solve(int n) {
+	std::vector<std::vector<std::string>> solved_boards;
 	std::vector<std::string> initial_board(n);
 	std::fill(initial_board.begin(), initial_board.end(), std::string(n, '.'));
-	solve_recurse(initial_board, 0, n);
+	solve_recurse(initial_board, 0, n, solved_boards);
 	return solved_boards;
 }
 
 int main() {
-	cout << solve(10).size() << endl;
+	std::vector<std::vector<std::string>> solved_boards = solve(6);
 	for (std::vector<std::string> board : solved_boards) {
 		print_board(board);
 	}
+	cout << solved_boards.size() << endl;
 	return 0;
 }
 
 /**
 	 Much better overall solution.
-	 Main improvements are removing looking and only checking
+	 Main improvements are removing looking ahead and only checking
 	 backwards, as well as a lot less faffing around with data
 	 structure formatting.
-	 Could use a bit of cleaning up, but otherwise good.
  **/
